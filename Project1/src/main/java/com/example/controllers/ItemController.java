f1f3b0ceb8ff5b25a6ccbdb445f50d541f22ddea
Ramandeep Singh <singh_ramandeep@live.com>
2020-04-01 23:13:31 +0530

    update web project

--- Project1/src/main/java/com/example/controllers/ItemController.java
+++ Project1/src/main/java/com/example/controllers/ItemController.java
@@ -2,13 +2,20 @@ package com.example.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ItemController {
 	
 	@RequestMapping("print")
-	public void print() {
-		System.out.println("Inside print method");
+	public ModelAndView print(@RequestParam("name") String myName) {
+		System.out.println("Inside print method" + myName);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("nameatt", myName);
+		mv.setViewName("home");
+		return mv;
+		
 	}
 
 }
